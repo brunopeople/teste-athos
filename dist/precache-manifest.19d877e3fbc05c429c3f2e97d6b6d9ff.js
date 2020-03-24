38128b4b3827b995a5936d0804691ab157535284
Bruno Pessoa <brunopessoa.melo@gmail.com>
2020-03-23 21:54:15 -0300

    subindo o projeto

---
+++ dist/precache-manifest.19d877e3fbc05c429c3f2e97d6b6d9ff.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89289b7b3b42e4b9b002",
+    "url": "/css/app.27803edc.css"
+  },
+  {
+    "revision": "52cb1088724b9f71c675",
+    "url": "/css/chunk-vendors.6a5e6c3f.css"
+  },
+  {
+    "revision": "e6b7e524fc15f61b8033c382f1f7e0da",
+    "url": "/data-sources/avatars/andy-lee-642320-unsplash.jpg"
+  },
+  {
+    "revision": "a6f1f670e35e2b4521eeb0c04de5b4c2",
+    "url": "/data-sources/avatars/annie-spratt-121576-unsplash.jpg"
+  },
+  {
+    "revision": "48f7f75c2e775c5004ce0886e5e6c5e2",
+    "url": "/data-sources/avatars/benjamin-parker-699522-unsplash.jpg"
+  },
+  {
+    "revision": "16a2e6c8d9505fd5d3cd9a42362f3ddc",
+    "url": "/data-sources/avatars/freestocks-org-190062-unsplash.jpg"
+  },
+  {
+    "revision": "924a58fb4e40f8bec6d5eabce2947744",
+    "url": "/data-sources/avatars/joshua-rawson-harris-432289-unsplash.jpg"
+  },
+  {
+    "revision": "b76eb8dd473607290ea874f8ef33983c",
+    "url": "/data-sources/avatars/nicolas-horn-689011-unsplash.jpg"
+  },
+  {
+    "revision": "63e8d58029d95b6ea85c0a92e24eca83",
+    "url": "/data-sources/clients.json"
+  },
+  {
+    "revision": "2d0a0d8f5f173be15a67aa084db94fe6",
+    "url": "/fonts/materialdesignicons-webfont.2d0a0d8f.eot"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "/fonts/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "/fonts/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "f51112347be6b44f9ef46151a971430d",
+    "url": "/fonts/materialdesignicons-webfont.f5111234.ttf"
+  },
+  {
+    "revision": "7298fabbecbc6f87cf690f717af8652c",
+    "url": "/img/justboil-logo.7298fabb.svg"
+  },
+  {
+    "revision": "e980652eaf87bfc6ec4e749d36d65485",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89289b7b3b42e4b9b002",
+    "url": "/js/app.8014933b.js"
+  },
+  {
+    "revision": "52cb1088724b9f71c675",
+    "url": "/js/chunk-vendors.224d883e.js"
+  },
+  {
+    "revision": "ec6b737ffed7db7023db",
+    "url": "/js/client-form.dfe2dc3c.js"
+  },
+  {
+    "revision": "53173e24e68551e211ab",
+    "url": "/js/client-form~forms.abb6d77f.js"
+  },
+  {
+    "revision": "8990c0166d28dd486532",
+    "url": "/js/forms.f4498940.js"
+  },
+  {
+    "revision": "7a599edb68b4dd97b069",
+    "url": "/js/profile.bda02ea8.js"
+  },
+  {
+    "revision": "bebff959d1eeae594e8d",
+    "url": "/js/tables.8adb3b94.js"
+  },
+  {
+    "revision": "9e02268cb14385af9653507bc8b8ee72",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
